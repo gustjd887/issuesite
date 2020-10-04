50dc2a883d5909f1a3e40bc0e774d532a5baaf72
gustjd887 <gustjd887@gmail.com>
2020-10-04 09:09:17 +0900

    delete secret_key

--- issuesite/settings.py
+++ issuesite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#veaflavw7mip$4dkhlj(^np!zpa1z3$f6lk)l3!#!td&0w3xr'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
