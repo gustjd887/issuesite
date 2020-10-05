873a6ee15e43a81276937d107ae519a927c3034d
gustjd887 <gustjd887@gmail.com>
2020-10-05 09:48:40 +0900

    change rank type Charfield to ArrayField

--- issue/models.py
+++ issue/models.py
@@ -1,19 +1,19 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 
 class Community(models.Model):
-    site_choice = (('pp', '뽐뿌'), ('cl', '클리앙'), ('ou', '오유'))
+    site_choice = (('pp', '뽐뿌'), ('cl', '클리앙'), ('ou', '오유')) # 선택할 수 있는 커뮤니티 목록 정의
 
-    site = models.CharField(max_length=10, choices=site_choice)
-    rank = models.CharField(max_length=100)
+    site = models.CharField(max_length=10, choices=site_choice) # 전체 랭킹을 위한 total이 추가되어 있으며, 이후 선택은 3가지중 하나
+    rank = ArrayField(ArrayField(models.IntegerField())) # 랭킹을 리스트 형태로 저장. Issue 클래스의 id를 저장한다.
 
     def __str__(self):
         return self.site
 
 
 class Issue(models.Model):
-    site = models.ForeignKey(Community, max_length=10, on_delete=models.CASCADE)
-    url = models.CharField(max_length=200, unique=True)
+    site = models.ForeignKey(Community, max_length=10, on_delete=models.CASCADE) # 커뮤니티 이름
+    url = models.CharField(max_length=200, unique=True) # 주소
     title = models.CharField(max_length=100)  # 제목
     reply = models.IntegerField()  # 댓글
     name = models.CharField(max_length=30)  # 닉네임 및 아이디
