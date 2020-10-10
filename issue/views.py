e0043588e7adcd40467b077fbe62c90c40e2ca7c
gustjd887 <gustjd887@gmail.com>
2020-10-10 23:27:23 +0900

    add ajax code for search

--- issue/views.py
+++ issue/views.py
@@ -7,12 +7,11 @@ from el_pagination.decorators import page_template
 def issue_list(request, template='main.html', extra_context=None):
     issue_list = Issue.objects.all()
     community_list = Community.objects.all()
-    # if request.path == '/filter/':  # 왼쪽 필터 적용 할 경우
     if request.GET.get('pp'):
         site_list = [site_key for site_key, site_value in request.GET.dict().items() if site_value == 'true']
         issue_list = issue_list.filter(site__site__in=site_list)
-    # elif request.path == '/search/':
-    #     issue_list = issue_list.filter(title__in=request.GET.get('keyword'))
+    elif request.GET.get('keyword'):
+        issue_list = issue_list.filter(title__contains=request.GET.get('keyword'))
     context = {
         'issue_list': issue_list.order_by('-date'),
         'community_list': community_list,
